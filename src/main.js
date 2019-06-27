88097c60a28e984abc0433ca147d78897dda1009
liuyuhua <1104101594@qq.com>
2019-06-27 22:44:05 +0800

    静态登录功能做好了

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+// 引入elementui
+import ElementUI from 'element-ui';
+// 引入elementui css
+import 'element-ui/lib/theme-chalk/index.css';
+import "./assets/css/index.css";
+
+// 安装elementui
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false;
 
 new Vue({
---
+++ src/views/Login.vue
@@ -0,0 +1,69 @@
+<template>
+  <el-row type="flex" class="row-bg" justify="center" align="middle">
+    <el-col :xs="14" :sm="12" :md="10" :lg="8" :xl="6">
+      <el-form ref="form" :model="form" :rules="rules" class="login-form" label-position="top" label-width="80px">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="form.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="form.password"></el-input>
+        </el-form-item>
+         <el-form-item>
+          <el-button type="primary" @click="submitForm('form')">登录</el-button>
+          <el-button @click="resetForm('form')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        form: {
+          username: '',
+          password: ''
+        },
+        rules: {
+          username: [
+            {required: true , message:' 请输入用户名', trigger: 'blur'},
+            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'change' }
+          ],
+          password: [
+            {required: true , message:' 请输入密码', trigger: 'blur'},
+            { min: 6, max: 8, message: '长度在 6 到 8 个字符', trigger: 'change' }
+          ]
+        }
+      }
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    }
+  }
+</script>
+
+<style>
+  .row-bg {
+    height: 100%;
+    background-color: #2D434C;
+  }
+  .login-form {
+    background-color: #fff;
+    padding: 30px 20px;
+    border-radius: 10px;
+    min-width: 400px;
+  }
+</style>
